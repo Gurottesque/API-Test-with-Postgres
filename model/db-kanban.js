2b1c53526bf7b77000eecdb644e4710c6dfe8497
Angel <a.bedoya3@utp.edu.co>
2024-07-07 18:19:51 -0500

    Actualizado

--- model/db-kanban.js
+++ model/db-kanban.js
@@ -8,3 +8,102 @@ export const pool = new pg.Pool({
     port: "5432"
 })
 
+
+export class KanbanDB {
+
+    
+    static async searchUser(username) {
+        try {
+            return await pool.query(`SELECT username FROM users WHERE username = $1`, [username])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    static async getPassword(username) {
+        try {
+            return await pool.query(`SELECT passwd FROM users WHERE username = $1`, [username])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    static async getID(username) {
+        try {
+            return await pool.query(`SELECT id FROM users WHERE username = $1`, [username])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+
+    static async getAllUserSections(value) {
+        try {
+            let userData = []
+            const sections_id = await pool.query(`SELECT title, id FROM section WHERE user_id = $1`, [value])
+            const objPromises = sections_id.rows.map( async (section) => {
+                const card = await pool.query('SELECT id, title, content FROM cards WHERE section_id = $1', [section.id])
+                const obj = {section_id : section.id, section_title : section.title, cards: card.rows}
+                userData.push(obj)
+                console.log(userData)
+            })
+            await Promise.all(objPromises);
+            return userData;
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    
+    static async getUserID(section_id) {
+        try {
+            return await pool.query(`SELECT user_id FROM section WHERE id = $1`, [section_id])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    static async getSectionID(card_id) {
+        try {
+            return await pool.query(`SELECT section_id FROM cards WHERE id = $1`, [card_id])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    static async addSection(title, user_id) {
+        try {
+            await pool.query(`INSERT INTO section (title, user_id) VALUES ($1, $2)`, [title, user_id]);
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+
+    static async addCards(title, content, section_id) {
+        try {
+            await pool.query(`INSERT INTO cards (title, content, section_id) VALUES ($1, $2, $3)`, [title, content, section_id]);
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+
+    static async addUser(username, hashedPasswd, email) {
+        try {
+            await pool.query(`INSERT INTO users (username, passwd, email) VALUES ($1, $2, $3)`, [username, hashedPasswd, email]);
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+
+
+    static async deleteSection(section_id) {
+        try {
+            return await pool.query(`DELETE FROM section WHERE id = $1`, [section_id])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+
+    static async deleteCard(card_id) {
+        try {
+            return await pool.query(`DELETE FROM cards WHERE id = $1`, [card_id])
+        } catch(e) { console.error(e); throw e }
+        
+    }
+}
--- model/db.sql
+++ model/db.sql
@@ -1,21 +1,21 @@
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(20) NOT NULL UNIQUE,
-    passwd VARCHAR(255) NOT NULL
+    username TXT NOT NULL UNIQUE,
+    passwd TEXT NOT NULL
 );
 
 CREATE TABLE section (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
-    title VARCHAR(56),
+    title TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE cards (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     section_id UUID NOT NULL,
-    title VARCHAR(255) NOT NULL,
+    title TEXT NOT NULL,
     content TEXT,
     FOREIGN KEY (section_id) REFERENCES section(id)
 );
