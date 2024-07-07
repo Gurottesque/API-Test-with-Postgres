cd5d59781de9f8ed9ebf6d137c90bd92d7fcf2f5
Angel <a.bedoya3@utp.edu.co>
2024-07-07 18:52:59 -0500

    Handling errors in main.js

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ app.use(cookieParser())
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
+// Manejo de errores de solicitud malformada y errores internos que no fueron capturados
 app.use((err, req, res, next) => {
     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
       return res.status(400).json({ error: 'Solicitud mal formada: JSON inválido' });
@@ -24,6 +25,10 @@ app.use((err, req, res, next) => {
     next();
   });
 
+app.use((err, req, res, next) => {
+    console.error('Error:', err);
+    res.status(500).json({ error: 'Error interno del servidor' });
+});
 
 app.listen(PORT)
 
